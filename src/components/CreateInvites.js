94576600a03195d44213aa2b395cc388d255aa2d
Grace Park <parkg748@newschool.edu>
2018-12-26 17:37:15 -0800

    finish functionalities in inviets page

--- src/components/CreateInvites.js
+++ src/components/CreateInvites.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import firebase from './firebase';
+import CreateTada from './CreateTada';
 
 class CreateInvites extends React.Component {
   constructor(props) {
@@ -11,8 +12,16 @@ class CreateInvites extends React.Component {
         email: this.props.email,
         channelDetail: this.props.channelDetail,
         channelsRef: firebase.database().ref('channels'),
+        invites: [],
+        close: ['none', 'none', 'none'],
+        idx: 3,
+        invitesValid: false
     };
     this.addChannel = this.addChannel.bind(this);
+    this.displayCloseButton = this.displayCloseButton.bind(this);
+    this.hideCloseButton = this.hideCloseButton.bind(this);
+    this.deleteInput = this.deleteInput.bind(this);
+    this.addMoreInput = this.addMoreInput.bind(this);
   }
 
   handleSubmit(e) {
@@ -34,12 +43,59 @@ class CreateInvites extends React.Component {
         }
     }
     channelsRef.child(key).update(newChannel);
-    this.setState({ detailValid: true });
+    this.setState({ invitesValid: true });
+  }
+
+  addInvitee(field, idx) {
+      return (e) => {
+        let temp = this.state.invites;
+        temp[idx] = e.target.value;
+        this.setState({ [field]: temp });
+      }
+  }
+
+  displayCloseButton(idx) {
+    let close = this.state.close;
+    close[idx] = '';
+    this.setState({ close });
+  }
+
+  hideCloseButton(idx) {
+    let close = this.state.close;
+    close[idx] = 'none';
+    this.setState({ close });
+  }
+
+  addMoreInput() {
+    let close = this.state.close;
+    close.push('none');
+    this.setState({ idx: this.state.idx + 1 });
+  }
+
+  deleteInput(idx) {
+    let invites = this.state.invites;
+    invites = [...invites.slice(0, idx), ...invites.slice(idx + 1)];
+    let close = this.state.close;
+    close = [...close.slice(0, idx), ...close.slice(idx + 1)];
+    this.setState({ idx: this.state.idx - 1, invites, close });
   }
 
   render() {
-    const { name, channelDetail } = this.state;
+    const { name, channelDetail, invites, close, idx, invitesValid } = this.state;
 
+    let inputBox = [];
+    let j = 105;
+    for (let i = 0; i < idx; i++) {
+        inputBox.push(<div className='invites-box' onMouseEnter={(e) => this.displayCloseButton(i)} onMouseLeave={(e) => this.hideCloseButton(i)}>
+            <input onChange={this.addInvitee('invites', i)} type='email' placeholder='name@example.com' value={invites[i]}/>
+            <i onClick={() => this.deleteInput(i)} style={{ display: `${close[i]}`, top: `${j}px` }} className='fas fa-times'></i>
+        </div>);
+        j += 60;
+    }
+
+    if (invitesValid) {
+        return <CreateTada name={name} channelDetail={channelDetail}/>;
+    }
     return (
       <div className='team-name'>
         <div className='get-started-header'>
@@ -50,16 +106,14 @@ class CreateInvites extends React.Component {
             <div className='teamname-left-sidebar'>
                 <div className='invites-left-sidebar-body'>
                     <h1>Who else is working on this project?</h1>
-                    <input type='email' placeholder='name@example.com'/>
-                    <input type='email' placeholder='name@example.com'/>
-                    <input type='email' placeholder='name@example.com'/>
+                    {inputBox}
                     <div className='invites-add-another'>
                         <i className="fas fa-plus-circle"></i>
-                        <span>Add another</span>
+                        <span onClick={() => this.addMoreInput()}>Add another</span>
                     </div>
                     <div className='invites-submit'>
                         <button>Add Teammates</button>
-                        <div className='invites-skipfornow'>Or, <Link to='/'>skip for now</Link></div>
+                        <div className='invites-skipfornow'>Or, <a onClick={(e) => this.handleSubmit(e)}>skip for now</a></div>
                     </div>
                     
                 </div>
