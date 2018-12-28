9ed780379e67595e34714ea7782e24894898643b
Grace Park <parkg748@newschool.edu>
2018-12-27 22:59:50 -0800

    add icon header to channels page

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -5,6 +5,8 @@ class Channels extends React.Component {
         super(props);
         this.state = {
             font: '#a09a9f',
+            edit: 'none',
+            search: '#717274'
         };
         this._onMouseEnter = this._onMouseEnter.bind(this);
         this._onMouseLeave = this._onMouseLeave.bind(this);
@@ -13,20 +15,28 @@ class Channels extends React.Component {
     _onMouseEnter(type) {
         if (type === 'font') {
             this.setState({ [type]: 'white' });
-        } 
+        } else if (type === 'edit') {
+            this.setState({ [type]: 'flex' });
+        } else if (type === 'search') {
+            this.setState({ [type]: '#0576b9' });
+        }
     }
 
     _onMouseLeave(type) {
         if (type === 'font') {
             this.setState({ [type]: '#a09a9f' });
+        } else if (type === 'edit') {
+            this.setState({ [type]: 'none' });
+        } else if (type === 'search') {
+            this.setState({ [type]: '#717274' });
         }
     }
 
     render() {
-        const { font} = this.state;
+        const { font, edit, search } = this.state;
 
         return (
-            <div>
+            <div className='channel'>
                 <div className='channel-left-sidebar'>
                     <div onMouseEnter={() => this._onMouseEnter('font')} onMouseLeave={() => this._onMouseLeave('font')} className='channel-left-header'>
                         <div className='channel-left-header-teamname'>
@@ -72,6 +82,55 @@ class Channels extends React.Component {
                             <div className='browse-apps-1'><i className="fas fa-caret-down"></i>Browse Apps</div>
                             <div className='browse-fa-plus-circle'><i className="fas fa-plus-circle"></i></div>
                             <div className='browse-apps-2'><i className="fas fa-caret-down"></i>Browse Apps</div>
+                        </div>
+                    </div>
+                </div>
+                <div className='channel-right-sidebar'>
+                    <div className='channel-header'>
+                        <div className='messages-header'>
+                            <div className='channel-name-container'>#testing</div>
+                            <div className='channel-header-info'>
+                                <div className='fa-star-container'><i className="far fa-star"></i></div>
+                                <div className='star-this-channel'><i className="fas fa-caret-up"></i>Star this channel</div>
+                                |
+                                <div className='fa-user-container'>
+                                    <i className="far fa-user"></i>1
+                                </div>
+                                <div className='view-member-list'><i className="fas fa-caret-up"></i>View member list</div>
+                                |
+                                <div className='fa-thumbtack-container'>
+                                    <i className="fas fa-thumbtack"></i>0
+                                </div>
+                                <div className='view-pinned-items'><i className="fas fa-caret-up"></i>View pinned items</div>
+                                |
+                                <div onMouseEnter={() => this._onMouseEnter('edit')} onMouseLeave={() => this._onMouseLeave('edit')} className='fa-pencil-alt-container-edit'>
+                                    <div className='fa-pencil-alt-container'>
+                                        <i className="fas fa-pencil-alt"></i>
+                                        Add a topic
+                                    </div>
+                                    <div style={{ display: `${edit}`}} className='add-topic-edit'>Edit</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='channel-title-info'>
+                            <i className="fas fa-phone"></i>
+                            <div className='phone-offline'><i className="fas fa-caret-up"></i>Only paid workspaces can start calls from channels.</div>
+                            <i className="fas fa-info-circle"></i>
+                            <div className='show-channel-details'><i className="fas fa-caret-up"></i>Show Channel Details</div>
+                            <i className="fas fa-cog"></i>
+                            <div className='channel-settings'><i className="fas fa-caret-up"></i>Channel Settings</div>
+                        </div>
+                        <div className='flex-header'>
+                            <input onMouseEnter={() => this._onMouseEnter('search')} onMouseLeave={() => this._onMouseLeave('search')} style={{ color: `${search}` }} type='text' placeholder='Search'/>
+                            <i style={{ color: `${search}` }} className="icon-search-wrapper fas fa-search"></i>
+                            <div className='channel-header-icon'>
+                                <i className="fas fa-at"></i>
+                                <div className='show-activity'><i className="fas fa-caret-up"></i>Show Activity</div>
+                                <i className="flex-header-star far fa-star"></i>
+                                <div className='show-starred-items'><i className="fas fa-caret-up"></i>Show Starred Items</div>
+                                <i className="fas fa-ellipsis-v"></i>
+                                <div className='more-items'><i className="fas fa-caret-up"></i>More Items</div>
+                            </div>
                         </div>
                     </div>
                 </div>
